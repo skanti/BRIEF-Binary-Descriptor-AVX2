e496e0d7e5c1d671acb12f5a93d0e17557e65d54
Armen Avetisyan <armen.avetisyan.to@gmail.com>
2016-08-12 23:17:19 +0900

    Templating 'rbrief' class method. Moving its definition to header.

--- example/main.cpp
+++ example/main.cpp
@@ -20,14 +20,18 @@
 #define CV_LOAD_IMAGE_X 1
 #endif
 
+struct Corners {
+    int x, y;
+    float angle;
+};
 
-void create_synthetic_data(std::vector<int> &x, std::vector<int> &y, std::vector<float> &angle, int n_features) {
+void create_synthetic_data(Corners *corners, int n_features) {
     std::mt19937 mt(999);
     std::uniform_real_distribution<float> u_dist(0, 1);
     for (int i = 0; i < n_features; i++) {
-        x[i] = (int) (u_dist(mt) * WIDTH_IMAGE);
-        y[i] = (int) (u_dist(mt) * HEIGHT_IMAGE);
-        angle[i] = u_dist(mt) * 3.14f;
+        corners[i].x = (int) (u_dist(mt) * WIDTH_IMAGE);
+        corners[i].y = (int) (u_dist(mt) * HEIGHT_IMAGE);
+        corners[i].angle = u_dist(mt) * 3.14f;
     }
 }
 
@@ -40,22 +44,20 @@ int main() {
 
     int n_dim_vec = N_DIM_BINARYDESCRIPTOR / SIZE_BITS_HAMING;
     int n_features = 500;
-    std::vector<int> x(n_features), y(n_features);
-    std::vector<float> angle(n_features);
-    create_synthetic_data(x, y, angle, n_features);
+    std::vector<Corners> corners(n_features);
+    create_synthetic_data(corners.data(), n_features);
     Matrix<int64_t> bd(n_dim_vec, n_features);
     double t_total = 0;
     for (int i = 0; i < n; i++) {
         cv::Mat image = cv::imread(dir + img_basenames[i], CV_LOAD_IMAGE_X);
-        BRIEF bd(4,n_features);
+        BRIEF bd(4, n_features);
         Timer::start();
-        bd.rbrief(image.data, HEIGHT_IMAGE, WIDTH_IMAGE, N_CHANNELS, STRIDE_IMAGE, x.data(), y.data(), angle.data(),
-                      n_features);
+        bd.rbrief(image.data, HEIGHT_IMAGE, WIDTH_IMAGE, N_CHANNELS, STRIDE_IMAGE, corners.data(), n_features);
         Timer::stop();
         double t = Timer::get_timing_in_ms();
         t_total += t;
         std::cout << "timing (ms): " << t << std::endl;
-        std::cout << std::bitset<64>(bd.bd(0,0)).to_string() << std::endl;
+        std::cout << std::bitset<64>(bd.bd(0, 0)).to_string() << std::endl;
     }
     std::cout << "timing average(ms): " << t_total / n << std::endl;
 }
